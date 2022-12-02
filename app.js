540809ebeedd25ba8c61714828bfd98544bbff3e
koziu33en <wiktor.szymczyk221@gmail.com>
2022-12-02 21:22:06 +0000

    Completed all exercises

--- app.js
+++ app.js
@@ -23,10 +23,27 @@ app.get("/animals/:pet_type", (req, res, next) => {
   const html = `<h1>List of ${
     pet_type.charAt(0).toUpperCase() + pet_type.slice(1)
   }</h1> <ul>${animals[pet_type]
-    .map((animal, index) => `<li>${animal.name}</li>`)
+    .map(
+      (animal, index) =>
+        `<li><a href=/animals/${pet_type}/${index}>${animal.name}</a></li>`
+    )
     .join("")} </ul>`;
 
   res.send(html);
+});
+
+app.get("/animals/:pet_type/:pet_id", (req, res, next) => {
+  const { pet_type, pet_id } = req.params;
+  const pet = animals[pet_type][pet_id];
+  res.send(
+    `<h1>Name: ${pet.name}</h1>
+    <img src=${pet.url} alt='' />
+    <p>Desc: ${pet.description}</p>
+    <ul>
+        <li>${pet.breed}</li>
+        <li>${pet.age}</li>
+    </ul>`
+  );
 });
 
 app.listen(port, () => {
